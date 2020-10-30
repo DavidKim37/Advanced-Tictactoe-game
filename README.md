74d41c6a7601eee6ce1626df4f5b99b07b5d63b5
David Kim <davidhkim1102@gmail.com>
2020-10-30 01:16:54 -0700

    updated to fit intellij

---
+++ README.md
@@ -0,0 +1,40 @@
+# Advanced Tic Tac Toe!
+
+This program enables players to enjoy an obscure version of tic tac toe. Althought not finished, there will eventually be options to choose your own color.
+
+## How to use
+
+After running the code, you should see a blank screen that says "Enter to start." Press enter, and you will begin the game! Please refer to the rules if you would like to learn how to play (not implemented).
+
+## File Descriptions
+
+#### Note - Please ignore the weird file names
+
+### General files
+
+main.py - Many functions defined here, runs the starting menu which links to other files
+
+README.md - Starting file detailing our program
+
+### Code files
+
+
+movepiece1.py - Contains the code that lets the user choose a space to go in, as well as the logic to deduce squares that are won.
+
+printboard.py - Prints the board that shows the spaces that each player has captured.
+
+### Other text files
+
+zzzdump.txt - Pieces of our code we may need later (ex. testing things, old portions that may have been replaced, saving strings/data)
+
+rules.txt - File the program reads when someone enters "3. Rules" in settings.  This is just to make the code itself less cluttered with a long string.
+
+david's code playground.py - A useless file that David put for no reason.
+
+## Creators
+
+DNHS AP CS Principles, Taught by Mr. Mortenson:
+Group Colin A - Pair Share 1
+
+David Kim
+Kyle Myint
--- printboard.py
+++ printboard.py
@@ -1,42 +1,42 @@
-#import os
+# import os
 
-def printboard(board,currentblock,dead):
-  #os.system("clear")
-  for i in board:
-    if i[0:2] == "Cc" or i[0:2] == "Fc" or i[0:2] == "Ic":
-      print(board[i] + "  ", end = i[2])
-      if i != "Ic1":
-        if i[2] == "1":
-          print("\n\u001B[107m                                     \u001B[0m")
-        else:
-          print(end = "\n")
-          for k in range(-2,1):
-            x = chr(ord(i[0])+k)
-            if x not in dead:
-              if x != currentblock:
-                print(end = "---|---|---")
-              else:
-                print(end = "\u001B[92m---|---|---\u001B[0m")
+def printboard(board, currentblock, dead):
+    # os.system("clear")
+    for i in board:
+        if i[0:2] == "Cc" or i[0:2] == "Fc" or i[0:2] == "Ic":
+            print(board[i] + "  ", end=i[2])
+            if i != "Ic1":
+                if i[2] == "1":
+                    print("\n\u001B[107m                                     \u001B[0m")
+                else:
+                    print(end="\n")
+                    for k in range(-2, 1):
+                        x = chr(ord(i[0]) + k)
+                        if x not in dead:
+                            if x != currentblock:
+                                print(end="---|---|---")
+                            else:
+                                print(end="\u001B[92m---|---|---\u001B[0m")
+                        else:
+                            print(board[x + "b2"][0:5], end="           \u001B[0m")
+                        if x != i[0]:
+                            print(end="\u001B[107m  \u001B[0m")
+                        else:
+                            print()
             else:
-              print(board[x+"b2"][0:5], end = "           \u001B[0m")
-            if x != i[0]:
-              print(end = "\u001B[107m  \u001B[0m")
-            else:
-              print()
-      else:
-        print("\n","a   b   c    a   b   c    a   b   c")
-    else:
-      if i[1] == "c":
-        print(board[i], end = "\u001B[107m  \u001B[0m")
-      else:
-        if i[0] == currentblock:
-          print("\u001B[92m" + board[i], end = "\u001B[92m|\u001B[0m")
+                print("\n", "a   b   c    a   b   c    a   b   c")
         else:
-          if i[0] in dead:
-            print(board[i] + board[i][0:5], end = " \u001B[0m")
-          else:
-            print(board[i], end = "|")
-  print("\n") # formatting
+            if i[1] == "c":
+                print(board[i], end="\u001B[107m  \u001B[0m")
+            else:
+                if i[0] == currentblock:
+                    print("\u001B[92m" + board[i], end="\u001B[92m|\u001B[0m")
+                else:
+                    if i[0] in dead:
+                        print(board[i] + board[i][0:5], end=" \u001B[0m")
+                    else:
+                        print(board[i], end="|")
+    print("\n")  # formatting
 
-
-#notes: 31 pink, 32 green, 33 orange, 34 darkest blue, 35 purple, 36 lighter but not light blue, 91 red, 92 slightly less neon green, 93 orangey yellow, 94 blue, 95 lavender, 96 electric blue
+# notes: 31 pink, 32 green, 33 orange, 34 darkest blue, 35 purple, 36 lighter but not light blue, 91 red, 92 slightly
+# less neon green, 93 orangey yellow, 94 blue, 95 lavender, 96 electric blue
